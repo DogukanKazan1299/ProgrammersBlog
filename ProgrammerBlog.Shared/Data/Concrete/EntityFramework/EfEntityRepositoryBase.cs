6e2899955421fa8add286a9364e98f08ed1aab86
DoğukanKazan <kazand25@gmail.com>
2022-01-10 18:13:10 +0300

    EfEntityRepositoryBase coding methods; 1-AddAsync 2-AnyAsync 3-CountAsync 4-DeleteAsync 5-GetAllAsync 6-GetAsync 7-UpdateAsync

---
+++ ProgrammerBlog.Shared/Data/Concrete/EntityFramework/EfEntityRepositoryBase.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProgrammerBlog.Shared.Data.Abstract;
+using ProgrammerBlog.Shared.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammerBlog.Shared.Data.Concrete.EntityFramework
+{
+    public class EfEntityRepositoryBase<TEntity> : IEntityRepository<TEntity>
+        where TEntity : class, IEntity, new()
+    {
+        private readonly DbContext _context;
+        public EfEntityRepositoryBase(DbContext context)
+        {
+            _context = context;
+        }
+        public async Task AddAsync(TEntity entity)
+        {
+            await _context.Set<TEntity>().AddAsync(entity);
+        }
+
+        public async Task<bool> AnyAsync(Expression<Func<TEntity, bool>> filter)
+        {
+            return await _context.Set<TEntity>().AnyAsync(filter);
+        }
+
+        public async Task<int> CountAsync(Expression<Func<TEntity, bool>> filter)
+        {
+            return await _context.Set<TEntity>().CountAsync(filter);
+        }
+
+        public async Task DeleteAsync(TEntity entity)
+        {
+            await Task.Run(() => { _context.Set<TEntity>().Remove(entity); });
+        }
+
+        public async Task<IList<TEntity>> GetAllAsync(Expression<Func<TEntity, bool>> filter, params Expression<Func<TEntity, object>>[] includeProperties)
+        {
+            IQueryable<TEntity> query = _context.Set<TEntity>();
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+            if (includeProperties.Any())
+            {
+                foreach (var includeProperty in includeProperties)
+                {
+                    query = query.Include(includeProperty);
+                }
+            }
+            return await query.ToListAsync();
+        }
+
+        public async Task<TEntity> GetAsync(Expression<Func<TEntity, bool>> filter, params Expression<Func<TEntity, object>>[] includeProperties)
+        {
+            IQueryable<TEntity> query = _context.Set<TEntity>();
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+            if (includeProperties.Any())
+            {
+                foreach (var includeProperty in includeProperties)
+                {
+                    query = query.Include(includeProperty);
+                }
+            }
+            return await query.SingleOrDefaultAsync();
+        }
+
+        public async Task UpdateAsync(TEntity entity)
+        {
+            await Task.Run(() => { _context.Set<TEntity>().Update(entity); });
+        }
+    }
+}
