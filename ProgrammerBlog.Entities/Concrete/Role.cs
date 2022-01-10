e680c4b87163a26043de813eaf927dad947266d5
DoğukanKazan <kazand25@gmail.com>
2022-01-10 15:44:59 +0300

    add entities 1-article 2-category 3-comment 4-role 5-user

---
+++ ProgrammerBlog.Entities/Concrete/Role.cs
@@ -0,0 +1,16 @@
+﻿using ProgrammerBlog.Shared.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammerBlog.Entities.Concrete
+{
+    public class Role:EntityBase,IEntity
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public ICollection<User> Users { get; set; }
+    }
+}
