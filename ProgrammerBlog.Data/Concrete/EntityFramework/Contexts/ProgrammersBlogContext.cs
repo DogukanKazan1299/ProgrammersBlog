9d80e4639e370efc6e516fbd4f55a655f6c9ca46
DoğukanKazan <kazand25@gmail.com>
2022-01-10 18:31:27 +0300

    Add context by data layer ProgrammersBlogContext coding DbSet and connectionString

---
+++ ProgrammerBlog.Data/Concrete/EntityFramework/Contexts/ProgrammersBlogContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProgrammerBlog.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammerBlog.Data.Concrete.EntityFramework.Contexts
+{
+    public class ProgrammersBlogContext : DbContext
+    {
+        public DbSet<Article> Articles { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-6AC6160; Database=ProgrammersBlogDatabase; integrated security=true");
+        }
+
+    }
+}
