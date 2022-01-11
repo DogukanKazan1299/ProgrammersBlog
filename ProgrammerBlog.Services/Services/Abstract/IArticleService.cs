6997402ad1facf10db6c5b61a0da9a26aa4e68a4
DoğukanKazan <kazand25@gmail.com>
2022-01-11 22:07:02 +0300

    IArticleService coding and Dto classes added

---
+++ ProgrammerBlog.Services/Services/Abstract/IArticleService.cs
@@ -0,0 +1,23 @@
+﻿using ProgrammerBlog.Entities.Dtos;
+using ProgrammerBlog.Shared.Utilities.Results.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammerBlog.Services.Services.Abstract
+{
+    public interface IArticleService
+    {
+        Task<IDataResult<ArticleListDto>> GetAll();
+        Task<IDataResult<ArticleDto>> Get(int articleId);
+        Task<IDataResult<ArticleListDto>> GetAllByNonDeleted();
+        Task<IDataResult<ArticleListDto>> GetAllByNonDeletedAndActive();
+        Task<IDataResult<ArticleListDto>> GetAllByCategory(int categoryId);
+        Task<IResult> Add(ArticleAddDto articleAddDto, string createdByName);
+        Task<IResult> Update(ArticleUpdateDto articleUpdateDto, string modifiedByName);
+        Task<IResult> Delete(int articleId,string modifiedByName);
+        Task<IResult> HardDelete(int articleId);
+    }
+}
