4dcbb303de664cabb67fb026f5371c9d968d42a5
DoğukanKazan <kazand25@gmail.com>
2022-01-12 16:06:59 +0300

    ServiceCollectionExtension added.

---
+++ ProgrammerBlog.Services/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using ProgrammerBlog.Data.Abstract;
+using ProgrammerBlog.Data.Concrete;
+using ProgrammerBlog.Data.Concrete.EntityFramework.Contexts;
+using ProgrammerBlog.Services.Services.Abstract;
+using ProgrammerBlog.Services.Services.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammerBlog.Services.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection LoadMyServices(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddDbContext<ProgrammersBlogContext>();
+            serviceCollection.AddScoped<IUnitOfWork, UnitOfWork>();
+            serviceCollection.AddScoped<ICategoryService, CategoryManager>();
+            serviceCollection.AddScoped<IArticleService, ArticleManager>();
+            return serviceCollection;
+        }
+    }
+}
