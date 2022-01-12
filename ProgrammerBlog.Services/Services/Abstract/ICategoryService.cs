d2c29578e6216e24b2ed5f360f5515cdd6ae072d
DoğukanKazan <kazand25@gmail.com>
2022-01-12 15:48:08 +0300

    CategoryManager is mapping and ICategoryService updated. 1-categoryDto added 2-categoryListDto added

--- ProgrammerBlog.Services/Services/Abstract/ICategoryService.cs
+++ ProgrammerBlog.Services/Services/Abstract/ICategoryService.cs
@@ -11,9 +11,10 @@ namespace ProgrammerBlog.Services.Services.Abstract
 {
     public interface ICategoryService
     {
-        Task<IDataResult<IList<Category>>> GetAll();
-        Task<IDataResult<Category>> Get(int categoryId);
-        Task<IDataResult<IList<Category>>> GetAllByNonDeleted();//silinmeyen kategorileri getir.
+        Task<IDataResult<CategoryListDto>> GetAll();
+        Task<IDataResult<CategoryDto>> Get(int categoryId);
+        Task<IDataResult<CategoryListDto>> GetAllByNonDeleted();//silinmeyen kategorileri getir.
+        Task<IDataResult<CategoryListDto>> GetAllByNonDeletedAndActive();
         Task<IResult> Add(CategoryAddDto categoryAddDto,string createdByName);
         Task<IResult> Update(CategoryUpdateDto categoryUpdateDto,string modifiedByName);
         Task<IResult> Delete(int categoryId, string modifiedByName);
